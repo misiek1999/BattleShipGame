4d6a0b15b835cceccafce28188a182b63682b272
misiek1999 <michaldydo99@gmail.com>
2025-12-17 23:10:42 +0100

    Change player type name in board player type. Add helper methods for board player type

--- lib/player_type/include/player_type.h
+++ lib/player_type/include/player_type.h
@@ -3,9 +3,25 @@
 #include <cstdint>
 
 enum class BoardPlayerType : uint8_t {
-    X = 0,
-    O = 1
+    Player_1 = 0,
+    Player_2 = 1
 };
+
+inline constexpr const char* board_player_type_to_string(const BoardPlayerType player_type) {
+    switch (player_type) {
+        case BoardPlayerType::Player_1: return "Player 1";
+        case BoardPlayerType::Player_2: return "Player 2";
+        default: return "Unknown Player";
+    }
+}
+
+inline constexpr BoardPlayerType get_opponent_player(const BoardPlayerType player_type) {
+    switch (player_type) {
+        case BoardPlayerType::Player_1: return BoardPlayerType::Player_2;
+        case BoardPlayerType::Player_2: return BoardPlayerType::Player_1;
+        default: throw std::invalid_argument("Invalid player type");
+    }
+}
 
 enum class PlayerError {
     NONE,
